fe6bf1d9937895eec245ee34fff8acc0572555e9
Admin <admin@Admins-MacBook-Air.local>
2021-06-04 09:13:56 +0100

    Switch between tabs Menu

---
+++ src/Menu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+const Menu = ({ items }) => {
+
+    return (
+        <div>
+            {items.map((menuItem) => {
+                const { id, title, price, img, desc } = menuItem;
+                return (
+                    <article className="menu-item" key={id}>
+                        <header>
+                            <h4>{title}</h4>
+                            <h4 className="price" >${price}</h4>
+                        </header>
+                        <div className="item-info" >
+                            <img src={img} alt={title} className="photo" />
+                            <p className="item-text" >{desc}</p>
+                        </div>
+                    </article>
+                );
+            })}
+        </div>
+    );
+};
+
+    export default Menu;
